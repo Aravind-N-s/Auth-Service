c3a2fad541fa4861c0c204cf505cbd75881e4409
Asura <aravind1808@gmail.com>
2020-02-20 11:06:54 +0530

    Logger

--- config/routes.js
+++ config/routes.js
@@ -6,7 +6,6 @@ const usercontroller = require('../controller')
 //user
 router.post('/register', usercontroller.register)
 router.post('/login',passport.authenticate('local',{session:false}),usercontroller.login)
-// router.put('/account/edit',passport.authenticate('local',{session: false}), usercontroller)
 router.get('/account',passport.authenticate('jwt',{session:false}), usercontroller.account)
 
 module.exports = router
--- middlewares/passport-local.js
+++ middlewares/passport-local.js
@@ -1,23 +1,31 @@
-const passport = require('passport')
-const {User} = require('../models')
-const bcryptjs = require('bcryptjs')
-const options ={
-    usernameField: 'email',
-    passwordField: 'password'
-}
+const { logger } = require("../config/logger");
+const passport = require("passport");
+const { User } = require("../models");
+const bcryptjs = require("bcryptjs");
+const options = {
+  usernameField: "email",
+  passwordField: "password"
+};
 
-const LocalStrategy = require('passport-local').Strategy
-passport.use(new LocalStrategy(options,(email, password, done) => {
-    User.findOne({email})
-    .then(user =>{
-        if(!user){
-            return done(null,'error')
-        }
-        bcryptjs.compare(password, user.password)
+const LocalStrategy = require("passport-local").Strategy;
+passport.use(
+  new LocalStrategy(options, (email, password, done) => {
+    User.findOne({ email }).then(user => {
+      logger.info(`${email} has sent a password request`);
+      if (!user) {
+        return done(null, "error");
+      }
+      bcryptjs
+        .compare(password, user.password)
         .then(result => {
-            if (result) {return done(null, user)}
-            done()
+          if (result) {
+            return done(null, user);
+          }
+          return done(null, false);
         })
-        .catch(err => {return done(err)})
-    })
-}))
+        .catch(err => {
+          return done(err);
+        });
+    });
+  })
+);
