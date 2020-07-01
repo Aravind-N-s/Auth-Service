a562f620cf960fbefd03845b92538203b0b9c112
Asura <aravind1808@gmail.com>
2020-07-01 20:23:25 +0530

    services

--- test/user.test.js
+++ test/user.test.js
@@ -1,23 +1,23 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const { User } = require("../models");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-// const server = require("../index");
+const server = require("../index");
 const should = chai.should();
 chai.use(chaiHttp);
 
 describe("Testing Travis", () => {
   it("Print Data", (done) => {
-    // chai
-    //   .request(server)
-    //   .get("/")
-    //   .end((err, res) => {
-    //     res.should.have.status(200);
-    //     res.body.should.be.a("object");
-    //     res.body.should.have
-    //       .property("message")
-    //       .eql(".AuthServices is active.");
-    //   });
+    chai
+      .request(server)
+      .get("/")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have
+          .property("message")
+          .eql(".AuthServices is active.");
+      });
     done();
   });
 });
